571a17ed3b12f542d896ba072e9d3eaed8f9b7a3
Nikhil Nair <nair.n@husky.neu.edu>
2019-03-16 13:15:48 -0400

    Removed default files and added sub directories

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div className="wbdv-root">
+        {/*<Provider store={store}>
+            <WidgetListContainer/>
+        </Provider>*/}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    </div>,
+    document.getElementById("root")
+
+);
