72323cfbc3c9b9a36287444a0cdc0528a4324eba
Nikhil Nair <nair.n@husky.neu.edu>
2019-04-24 03:18:02 -0400

    Clicking on a news article on the main page takes you to the details page

--- src/components/News/NewsCarousel.js
+++ src/components/News/NewsCarousel.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import {Redirect} from 'react-router';
 import './news.css'
+import {withRouter} from 'react-router';
 
-export default class NewsCarousel extends React.Component {
+class NewsCarousel extends React.Component {
     constructor(props, context) {
         super(props, context);
 
@@ -21,6 +22,20 @@ export default class NewsCarousel extends React.Component {
             direction: e.direction,
         });
     }
+
+    navigate = (title, description, content, url, urlToImage, date, author ) =>
+        this.props.history.push({
+            pathname:'/searchItem',
+            state: {
+                title: title,
+                description: description,
+                content: content,
+                url: url,
+                urlToImage: urlToImage,
+                date: date,
+                author: author
+            }
+        })
 
     render() {
         const {index, direction} = this.state;
@@ -40,7 +55,11 @@ export default class NewsCarousel extends React.Component {
                                          src={article.urlToImage}
                                     />
                                     <Carousel.Caption>
-                                        <a href={article.url} target="_blank">
+                                        <a  className="article-link"
+                                            onClick={() => this.navigate(article.title,article.description,article.content,
+                                            article.url, article.urlToImage, article.publishedAt, article.author
+                                        )}
+                                            >
                                             <p className="article-text">{article.title}</p>
                                         </a>
                                     </Carousel.Caption>
@@ -52,3 +71,5 @@ export default class NewsCarousel extends React.Component {
         );
     }
 }
+
+export default withRouter(NewsCarousel)
